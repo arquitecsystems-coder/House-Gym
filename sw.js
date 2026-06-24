386be1c5030726484764f1c9f62811ce32aead4b
arquitecsystems-coder <arquitecsystems@gmail.com>
2026-06-24 09:42:06 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = "forge-v2";
+const FILES = [
+  "/House-Gym/index.html",
+  "/House-Gym/manifest.json",
+  "/House-Gym/icon-192.png",
+  "/House-Gym/icon-512.png",
+  "/House-Gym/icon-180.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
